7c5019aac367e830b21fbbe0083eb227c44d2e1c
Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2025-09-24 18:18:50 +0600

    started render and commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import "./App.css";
-import ComponentsMemory from "./2.3/lesson-3/ComponentsMemory"; 
-import MultipleState from "./2.3/lesson-3/MultipleState";
+import "./App.css"; 
 
 function App() {
   return (
     <div className="h-screen w-full max-w-7xl mx-auto flex justify-center items-center">
-      {/* <ComponentsMemory /> */}
-      <MultipleState />
-      <MultipleState />
+       
     </div>
   );
 }
