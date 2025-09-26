4e0ade993824ed4dadafef6e3ebca25fb273adfd
Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2025-09-26 23:27:15 +0600

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import State from "./2.5/lesson-6/State";
 import "./App.css"; 
 
 function App() {
   return (
     <div className="h-screen w-full max-w-7xl mx-auto flex justify-center items-center">
 <<<<<<< HEAD
+<<<<<<< HEAD
       Run the index.html using live server
 =======
        
 >>>>>>> 2.5
+=======
+       <State />
+>>>>>>> fe962beac496a8ae8ee9b7ebbf753cb207ca47a6
     </div>
   );
 }
