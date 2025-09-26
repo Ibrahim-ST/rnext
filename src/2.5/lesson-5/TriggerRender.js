fe962beac496a8ae8ee9b7ebbf753cb207ca47a6
Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2025-09-26 23:17:40 +0600

    state initiated

--- src/2.5/lesson-5/TriggerRender.js
+++
@@ -1,6 +0,0 @@
-import Image from "./image";
-import { createRoot } from 'react-dom/client';
-
-
-const root = createRoot(document.getElementById('root'));
-root.render(<Image />)
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import State from "./2.5/lesson-6/State";
 import "./App.css"; 
 
 function App() {
   return (
     <div className="h-screen w-full max-w-7xl mx-auto flex justify-center items-center">
-       
+       <State />
     </div>
   );
 }
