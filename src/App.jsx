fe707275bb9d2c2fdbb9c69c6de341fb2d9f1f72
Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2025-09-28 11:27:29 +0600

    completed state as objects and updating dynamic states in form

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import State from './2.6/lesson-6/State';
 import Counter from './2.6/lesson-6/Counter';
 import StateOverTime from './2.6/lesson-6/StateOverTime';
+import BatchState from './2.7/lesson-7/BatchState';
+import UpdatingObjects from './2.8/lesson-8/UpdatingObjects';
+import SpreadingObjects from './2.8/lesson-8/SpreadingObjects';
+import DynamicStateChange from './2.8/lesson-8/DynamicStateChange';
 
 const App = () => {
   return (
-    <div className = 'h-screen w-full max-w-7xl mx-auto  flex justify-center items-center align-baseline' >
+    <div className = 'h-screen w-full max-w-7xl mx-auto  flex flex-col justify-center items-center ' >
       {/* <State /> */}
       {/* <Counter /> */}
-      <StateOverTime />
+      {/* <StateOverTime /> */}
+      {/* <BatchState /> */}
+      {/* <UpdatingObjects />  */}
+      {/* <SpreadingObjects /> */}
+      <DynamicStateChange />
     </div>
   );
 };
