520030d3aacca77df939b7764d43c6f08a4a023a
Md. Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2026-01-18 11:49:06 +0600

    using and providing context from same component

--- src/3.16/components/Section.jsx
+++ src/3.16/components/Section.jsx
@@ -1,9 +1,12 @@
 import { LevelContext } from "../contexts/LevelContext";
+import { useContext } from 'react';
 
-export default function Section({ level, children }) {
+export default function Section({ children }) {
+    const level = useContext(LevelContext);
+
   return (
     <section className="section">
-      <LevelContext.Provider value={level}>{children}</LevelContext.Provider>
+      <LevelContext.Provider value={level +1}>{children}</LevelContext.Provider>
     </section>
   );
 }
